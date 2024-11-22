a6a38d16fa55151397b3dec3b4533d1b08c6dd31
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2024-11-22 08:17:32 +0545

    home page changes

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,6 +1,7 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { PRODUCTS } from '../../../assets/products'
-import {ProductListItem} from "../components/product-list-item";
+import {ProductListItem} from "../../components/product-list-item";
+import { ListHeader } from '../../components/list-header';
 
 
 const Home = () => {
@@ -14,7 +15,7 @@ const Home = () => {
      } 
       keyExtractor={item=> item.id.toString()}
       numColumns={2}
-      ListHeaderComponent={<Text>Products</Text>}
+      ListHeaderComponent={ListHeader}
       contentContainerStyle={styles.flatListContent}
       columnWrapperStyle={styles.flatListColumn}
       style={{paddingHorizontal: 10, paddingVertical: 5 }}
