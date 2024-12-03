009c9a24d940634bb3ff581e51993b48c418c85e
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2024-12-03 20:58:33 +0545

    cart-store code and list-header code done

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { PRODUCTS } from "../../assets/products";
 
 type CartItemType = {
     id: number;
@@ -25,20 +26,60 @@ type CartState = {
 
 export const useCartStore = create<CartState>((set,get)=>({
     items: initialCartItems,
+
     addItem: (item: CartItemType) => {
         const existingItem =get().items.find(i=>i.id===item.id);
         if(existingItem) {
+          set(state => ({
+            items: state.items.map(i =>
+              i.id === item.id
+                ? {
+                    ...i,
+                    quantity: Math.min(i.quantity + item.quantity,PRODUCTS.find(p=>p.id===item.id)?.maxQuantity ||
+                  i.quantity),
+                  }
+                : i
+            ),
+          }));
 
         }  else {
          set(state => ({ items: [...state.items, item] }));
             
         }   
     },
-    removeItem:() =>null,
-    incrementItem:() =>null,
-    decrementItem:()=> null,
-    getTotalPrice:()=>'',
-    getItemCount:()=>0,
+    removeItem: (id: number) =>
+      set(state => ({ items: state.items.filter(item => item.id !== id) })),
+    incrementItem: (id: number) =>
+      set(state => {
+        const product=PRODUCTS.find(p=>p.id===id);
+        return {
+          items: state.items.map(item =>
+            item.id === id && item.quantity < product.maxQuantity
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }),
+    decrementItem: (id: number) =>
+      set(state => ({
+        items: state.items.map(item =>
+          item.id === id && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      })),
+    getTotalPrice: () => {
+      const { items } = get();
+  
+      return items
+        .reduce((total, item) => total + item.price * item.quantity, 0)
+        .toFixed(2);
+    },
+    getItemCount: () => {
+      const { items } = get();
+      return items.reduce((count, item) => count + item.quantity, 0);
+    },
+    resetCart: () => set({ items: initialCartItems }),
     
 
 }));
