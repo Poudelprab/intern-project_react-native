a1081424f8365bc543bfcc3344c2c10cc630b936
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2024-12-09 23:09:52 +0545

    database connections

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -2,28 +2,30 @@ import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { PRODUCTS } from '../../../assets/products'
 import {ProductListItem} from "../../components/product-list-item";
 import { ListHeader } from '../../components/list-header';
+import Auth from '../auth';
 
 
 const Home = () => {
   return (
-    <View>
-      <FlatList 
-      data = {PRODUCTS} 
-      renderItem={({ item }) =>
-        <ProductListItem product={item} />
+    
+     <View>
+       <FlatList 
+       data = {PRODUCTS} 
+       renderItem={({ item }) =>
+         <ProductListItem product={item} />
       
-     } 
-      keyExtractor={item=> item.id.toString()}
-      numColumns={2}
-      ListHeaderComponent={ListHeader}
-      contentContainerStyle={styles.flatListContent}
-      columnWrapperStyle={styles.flatListColumn}
-      style={{paddingHorizontal: 10, paddingVertical: 5 }}
+      } 
+       keyExtractor={item=> item.id.toString()}
+       numColumns={2}
+       ListHeaderComponent={ListHeader}
+       contentContainerStyle={styles.flatListContent}
+       columnWrapperStyle={styles.flatListColumn}
+       style={{paddingHorizontal: 10, paddingVertical: 5 }}
 
-      />
-    </View>
-  )
-}
+       />
+     </View>
+  );
+};
 
 export default Home
 
