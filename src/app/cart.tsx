fa33348944db1f5f1a927c6670a9e576d94e76af
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2024-12-09 10:35:28 +0545

    order, orderdetails fetched

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -11,18 +11,98 @@ import {
 import { useCartStore } from '../store/cart-store';
 import { StatusBar } from 'expo-status-bar';
 
+type CartItemType = {
+  id: number;
+  title: string;
+  heroImage: string;
+  price: number;
+  quantity: number;
+  maxQuantity: number;
+};
+
+type CartItemProps = {
+  item: CartItemType;
+  onRemove: (id: number) => void;
+  onIncrement: (id: number) => void;
+  onDecrement: (id: number) => void;
+};
+
+const CartItem = ({
+  item,
+  onDecrement,
+  onIncrement,
+  onRemove,
+}: CartItemProps) => {
+  return (
+    <View style={styles.cartItem}>
+      <Image source={{ uri: item.heroImage }} style={styles.itemImage} />
+      <View style={styles.itemDetails}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemPrice}>Rs {item.price.toFixed(2)}</Text>
+        <View style={styles.quantityContainer}>
+          <TouchableOpacity
+            onPress={() => onDecrement(item.id)}
+            style={styles.quantityButton}
+          >
+            <Text style={styles.quantityButtonText}>-</Text>
+          </TouchableOpacity>
+          <Text style={styles.itemQuantity}>{item.quantity}</Text>
+          <TouchableOpacity
+            onPress={() => onIncrement(item.id)}
+            style={styles.quantityButton}
+          >
+            <Text style={styles.quantityButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => onRemove(item.id)}
+        style={styles.removeButton}
+      >
+        <Text style={styles.removeButtonText}>Remove</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 export default function Cart()
 {
     const {items, removeItem, incrementItem, decrementItem, getTotalPrice} = useCartStore();
 
     const handleCheckout = () =>
     {
-        Alert.alert("Proceeding to checkout", `Total amount: $${getTotalPrice()}`)
+        Alert.alert("Proceeding to checkout", `Total amount: Rs ${getTotalPrice()}`)
     }
     return(
         <View style={styles.container}>
             <StatusBar style={Platform.OS==='android' ? 'light' : 'auto'}/>
-            <Text>Cart</Text>
+            {/*flatlist*/}
+
+            <FlatList
+        data={items}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <CartItem
+            item={item}
+            onRemove={removeItem}
+            onIncrement={incrementItem}
+            onDecrement={decrementItem}
+          />
+        )}
+        contentContainerStyle={styles.cartList}
+      />
+
+      <View style={styles.footer}>
+        <Text style={styles.totalText}>Total: Rs {getTotalPrice()}</Text>
+        <TouchableOpacity
+          onPress={handleCheckout}
+          style={styles.checkoutButton}
+        >
+          <Text style={styles.checkoutButtonText}>Checkout</Text>
+        </TouchableOpacity>
+      </View>
+
         </View>
     )
 }
